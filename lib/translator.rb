d419777c1b015b76a8a638d2183706ace0f24776
darien bryant <darienbryant11@gmail.com>
2020-12-03 17:28:11 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
-# require modules here
 
-def load_library
-  # code goes here
-end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
-def get_english_meaning
-  # code goes here
-end
+
+def load_library(emoticon.yml)
+  emoticons = YAML.load_file(emoticon_file)
+  emoticon_list = {"get_meaning" => {}, "get_emoticon" => {}} 
+  
+  emoticons.collect do |key, value|
+    emoticon_list["get_emoticon"][value[0]]
+ = value[1]  
+ emoticon_list["get_meaning"][value[1]] = key
+ end 
+ emoticon_list
+ end 
+ 
+ 
+ def get_japanese_emoticon
+   emoticons = YAML.load_file(emoticons.yml)
+
+ end 
+ 
+ def get_english_meaning
+   emoticons = YAML.load_file(emoticons.yml)
+
+  end 
+  
+  
+  
+  
+  
+  
+  
