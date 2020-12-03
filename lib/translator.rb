0cfacdf4df62393edce2f6dec6b1e2df76f607d6
darien bryant <darienbryant11@gmail.com>
2020-12-03 17:44:36 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,13 @@ def load_library(emoticon_list)
  
   emoticons = YAML.load_file(emoticon_list)
   
-  
+  key_hash= {}
+
   emoticons.collect do |key, value|
-key_hash = {}
+key_hash[key] = {}
       binding.pry
+key_hash = [key][value] 
+
 
     emoticon_list["get_emoticon"][value[0]] = value[1]  
  emoticon_list["get_meaning"][value[1]] = key
